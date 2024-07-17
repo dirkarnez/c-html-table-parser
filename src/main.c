2d87204a5a9510a4b4d7f19ebfe785155af12fa9
Dirk Arnez <smalldirkalex@gmail.com>
2024-07-18 03:21:09 +0800

    - update src/main.c

--- src/main.c
+++ src/main.c
@@ -4,6 +4,27 @@
 
 #define MAX_BUFFER_SIZE 1024
 
+typedef struct HtmlAttribute {
+    char* name;
+    char* value;
+    struct HtmlAttribute* next;
+} HtmlAttribute;
+
+typedef struct HtmlElement {
+    char* tag_name;
+    HtmlAttribute* attributes;
+    char* text_content;
+    struct HtmlElement* children;
+    struct HtmlElement* next;
+} HtmlElement;
+
+typedef struct HtmlTable {
+    HtmlElement* thead;
+    HtmlElement* tbody;
+    HtmlElement* tfoot;
+} HtmlTable;
+
+
 // Function to parse an HTML tag
 int parse_tag(char* buffer, char** tag_name, char** attributes) {
     char* start = buffer;
@@ -58,6 +79,165 @@ void parse_table(char* html) {
     }
 }
 
+HtmlAttribute* parse_attributes(char* attribute_str) {
+    HtmlAttribute* head = NULL;
+    HtmlAttribute* current = NULL;
+
+    char buffer[MAX_BUFFER_SIZE];
+    char* name = buffer;
+    char* value = NULL;
+    int i = 0, j = 0;
+
+    while (attribute_str[i]) {
+        if (attribute_str[i] == '=') {
+            buffer[j] = '\0';
+            name = buffer;
+            value = attribute_str + i + 1;
+            j = 0;
+            i++;
+            while (attribute_str[i] != ' ' && attribute_str[i] != '\0' && attribute_str[i] != '"') {
+                i++;
+            }
+            if (attribute_str[i] == '"') {
+                i++;
+                value = attribute_str + i;
+                while (attribute_str[i] != '"' && attribute_str[i] != '\0') {
+                    i++;
+                }
+            }
+            attribute_str[i] = '\0';
+            i++;
+
+            HtmlAttribute* attr = malloc(sizeof(HtmlAttribute));
+            attr->name = strdup(name);
+            attr->value = strdup(value);
+            attr->next = NULL;
+
+            if (head == NULL) {
+                head = current = attr;
+            } else {
+                current->next = attr;
+                current = attr;
+            }
+
+            j = 0;
+        } else if (attribute_str[i] == ' ') {
+            buffer[j] = '\0';
+            name = buffer;
+            value = NULL;
+            j = 0;
+            i++;
+
+            HtmlAttribute* attr = malloc(sizeof(HtmlAttribute));
+            attr->name = strdup(name);
+            attr->value = NULL;
+            attr->next = NULL;
+
+            if (head == NULL) {
+                head = current = attr;
+            } else {
+                current->next = attr;
+                current = attr;
+            }
+        } else {
+            buffer[j++] = attribute_str[i++];
+        }
+    }
+
+    if (j > 0) {
+        buffer[j] = '\0';
+        name = buffer;
+        value = NULL;
+
+        HtmlAttribute* attr = malloc(sizeof(HtmlAttribute));
+        attr->name = strdup(name);
+        attr->value = NULL;
+        attr->next = NULL;
+
+        if (head == NULL) {
+            head = current = attr;
+        } else {
+            current->next = attr;
+            current = attr;
+        }
+    }
+
+    return head;
+}
+
+void parse_html_table(char* html, HtmlTable* table) {
+   
+    table->thead = NULL;
+    table->tbody = NULL;
+    table->tfoot = NULL;
+
+    HtmlElement* current_element = NULL;
+    HtmlElement* current_row = NULL;
+
+    char buffer[MAX_BUFFER_SIZE];
+    char* tag_name;
+    char* attributes;
+    int i = 0;
+
+    while (html[i]) {
+        if (html[i] == '<') {
+            int len = parse_tag(html + i, &tag_name, &attributes);
+            if (len > 0) {
+                if (strcmp(tag_name, "table") == 0) {
+                    // Parse table attributes
+                } else if (strcmp(tag_name, "thead") == 0) {
+                    table->thead = current_element = malloc(sizeof(HtmlElement));
+                    current_element->tag_name = strdup(tag_name);
+                    current_element->attributes = parse_attributes(attributes);
+                    current_element->children = NULL;
+                    current_element->next = NULL;
+                } else if (strcmp(tag_name, "tbody") == 0) {
+                    table->tbody = current_element = malloc(sizeof(HtmlElement));
+                    current_element->tag_name = strdup(tag_name);
+                    current_element->attributes = parse_attributes(attributes);
+                    current_element->children = NULL;
+                    current_element->next = NULL;
+                } else if (strcmp(tag_name, "tfoot") == 0) {
+                    table->tfoot = current_element = malloc(sizeof(HtmlElement));
+                    current_element->tag_name = strdup(tag_name);
+                    current_element->attributes = parse_attributes(attributes);
+                    current_element->children = NULL;
+                    current_element->next = NULL;
+                } else if (strcmp(tag_name, "tr") == 0) {
+                    current_row = current_element = malloc(sizeof(HtmlElement));
+                    current_element->tag_name = strdup(tag_name);
+                    current_element->attributes = parse_attributes(attributes);
+                    current_element->children = NULL;
+                    current_element->next = NULL;
+                    if (table->thead) {
+                        table->thead->children = current_element;
+                    } else if (table->tbody) {
+                        table->tbody->children = current_element;
+                    } else if (table->tfoot) {
+                        table->tfoot->children = current_element;
+                    }
+                } else if (strcmp(tag_name, "th") == 0 || strcmp(tag_name, "td") == 0) {
+                    HtmlElement* cell = malloc(sizeof(HtmlElement));
+                    cell->tag_name = strdup(tag_name);
+                    cell->attributes = parse_attributes(attributes);
+                    cell->text_content = NULL;
+                    cell->children = NULL;
+                    cell->next = NULL;
+                    if (current_row) {
+                        current_row->children = cell;
+                        current_row = current_row->next;
+                    }
+                }
+                i += len;
+            } else {
+                i++;
+            }
+        } else {
+            i++;
+        }
+    }
+}
+
 int main() {
     char html[] = "<table style=\"width: 100%\">"
                   "  <tr>"
@@ -75,5 +255,11 @@ int main() {
                   "</table>";
 
     parse_table(html);
+
+    HtmlTable* table = malloc(sizeof(HtmlTable));
+
+    parse_html_table(html, table);
+
+    printf("%s", table->thead->tag_name);
     return 0;
 }
